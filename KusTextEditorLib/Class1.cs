8b4c31c30906619bf1695992189d59ab4acb2815
Kushal Hada <kushaldeveloper@gmail.com>
2020-11-21 19:22:11 -0500

    add more projects!

---
+++ KusTextEditorLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace KusTextEditorLib
+{
+    public class Class1
+    {
+    }
+}
---
+++ KusTextEditorTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace KusTextEditorTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
