629eb8a05b40d05887b43d5cff50e8f59db31b10
Kushal Hada <kushaldeveloper@gmail.com>
2020-11-21 19:22:56 -0500

    add a freebie test

--- KusTextEditorTests/UnitTest1.cs
+++ KusTextEditorTests/UnitTest1.cs
@@ -6,9 +6,9 @@ namespace KusTextEditorTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Freebie()
         {
-
+            Assert.Equal(1, 1);
         }
     }
 }
